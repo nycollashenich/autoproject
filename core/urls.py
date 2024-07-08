e69f072ecbf8d9e7c30f60ef20e9a6190461c98a
Nycollas <nhhenich@gmail.com>
2024-07-08 15:41:34 -0300

    pages error

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import IndexView, ContactView
+from .views import IndexView, ContactView, View404, View500
+from django.conf.urls import handler404, handler500
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'), #htopprojetos
     # path('projetos/', IndexView.as_view(), name='projects'),
     path('contato/', ContactView.as_view(), name='contact'),
 ]
+
+handler404 = View404.as_view()
+handler500 = View500.as_view()
--- core/views.py
+++ core/views.py
@@ -8,3 +8,15 @@ class IndexView(TemplateView):
 class ContactView(TemplateView):
     template_name = 'contact.html'
 
+
+
+
+# page error
+from django.views import View
+class View404(View): 
+    def get(self, request, *args, **kwargs):
+        return render(request, '404.html', status=404)
+
+class View500(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, '500.html', status=500)
